d9f9e00afd21b12b325c8cc5ccf86dcd22ab344a
davidcortesortuno <david.cortes.o@gmail.com>
2017-05-26 14:32:34 +0100

    Updated to 1.1 for PyPI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="nebm_plot_tools",
-    version="1.0",
+    version="1.1",
     author="D. I. Cortes",
     description='Plotting library for NEBM data based on Matplotlib and Mayavi',
     url='https://github.com/davidcortesortuno/nebm_plot_tools',
