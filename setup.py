2141d6310230909bc8c4b9b9c73b5284af59431f
davidcortesortuno <david.cortes.o@gmail.com>
2017-05-26 13:14:53 +0100

    Added setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="nebm_plot_tools",
+    version="1.0",
+    author="D. I. Cortes",
+    description='Plotting library for NEBM data based on Matplotlib and Mayavi',
+    url='https://github.com/davidcortesortuno/nebm_plot_tools',
+    install_requires=[
+        'matplotlib', 'mayavi', 'numpy'
+    ],
+    packages=['nebm_plot_tools'],
+    license='MIT',
+)
